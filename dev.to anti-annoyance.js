7a8c6d4ea39745066acf6926720bc4454cc78dc4
Zander Maxwell <zandermaxwell@hey.com>
2024-06-13 17:15:03 +0200

    Create dev.to anti-annoyance.js

---
+++ dev.to anti-annoyance.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Dev.to anti-annoyance
+// @namespace    http://tampermonkey.net/
+// @version      2024-06-13
+// @description  try to take over the world!
+// @author       zandermaxwell@hey.com
+// @match        https://dev.to/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=dev.to
+// @grant        none
+// ==/UserScript==
+
+const query = '[id^="sponsorship-close-trigger"]'
+
+const waitForIt = (selectorText, callback) => {
+  const observer = new MutationObserver(function (mutations, me) {
+    const element = document.querySelector(query);
+    if (element) {
+      me.disconnect(); // stop observing
+      callback();
+    }
+  });
+  observer.observe(document, {
+    childList: true,
+    subtree: true,
+  });
+};
+
+
+
+// Close big useless sponsonship notification
+const goAway = () => {
+    document.querySelector(query).click();
+};
+
+waitForIt(query, goAway);
+
